9ea05a3b5f26ca3b2a04d66ad259fa4a0b095d0e
lienhua34 <lienhua34@163.com>
2016-09-11 22:34:02 -0700

    使用空格缩进

--- .entry.el
+++ .entry.el
@@ -1,3 +1,8 @@
+;; 使用空格缩进
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq tab-width 4)
+
 ;; config-dir变量记录的是这个库clone到本地的路径，默认是“~/.emacs.”。
 ;; 如果不是这个目录，则需要进行修改，因为后续加载各种环境配置入口文件均需要该变量
 (setq root-config-dir "~/.emacs.d")
