29fc9fc1e98c69718462fd5051ec9674892b29bf
lienhua34 <lienhua34@163.com>
2017-05-12 15:45:49 +0800

    add auto-mode-list ("Jenkinsfile" . groovy-mode)

--- elisp/groovy/.entry.el
+++ elisp/groovy/.entry.el
@@ -2,6 +2,7 @@
 (require 'groovy-mode)
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 (add-to-list 'auto-mode-alist '("\\.groovy\\'" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\\Jenkinsfile\\'" . groovy-mode))
 
 (add-hook 'groovy-mode-hook
            '(lambda ()
